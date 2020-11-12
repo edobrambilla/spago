de53a28aa360cedaacb59e62c06fc5f231b8c252
Edoardo Brambilla <e.brambilla@advanced-analytics.eu>
2020-11-12 17:07:48 +0100

    Add data test

---
+++ pkg/utils/data/data_test.go
@@ -0,0 +1,11 @@
+// Copyright 2019 spaGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package data
+
+import "testing"
+
+func TestGenerateNGrams(t *testing.T) {
+
+}
