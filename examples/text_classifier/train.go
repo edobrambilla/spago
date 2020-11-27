47e9b4dc15c3bc368aacf1ae3c10320de00be873
Edoardo Brambilla <e.brambilla@advanced-analytics.eu>
2020-11-27 18:42:03 +0100

    Add learn for trainer

--- examples/text_classifier/train.go
+++ examples/text_classifier/train.go
@@ -6,11 +6,14 @@ package main
 
 import (
 	"github.com/nlpodyssey/spago/examples/text_classifier/trainer"
+	"github.com/nlpodyssey/spago/pkg/mat"
 	"github.com/nlpodyssey/spago/pkg/ml/nn"
 	"github.com/nlpodyssey/spago/pkg/ml/optimizers/gd"
 	"github.com/nlpodyssey/spago/pkg/ml/optimizers/gd/adam"
 	"github.com/nlpodyssey/spago/pkg/nlp/classifiers/prado"
+	"github.com/nlpodyssey/spago/pkg/nlp/vocabulary"
 	"log"
+	"math"
 	"net/http"
 	"os"
 )
@@ -38,9 +41,14 @@ func main() {
 		ModelPath:        modelPath,
 	}
 	t := trainer.NewTrainer(model, config, optimizer)
-
+	//get vocabulary
+	v := t.GetVocabulary()
+	vocabularyCodes := getHashedVocabulary(v)
+	model.Vocabulary = v
+	model.Embeddings.SetProjectedEmbeddings(vocabularyCodes)
+	print(v.Size())
 	// read dataset
-	t.Train()
+	t.Enjoy()
 
 }
 
@@ -64,13 +72,41 @@ func newTestModel() *prado.Model {
 		TypeVocabSize:         0,
 		VocabSize:             5,
 		Id2Label: map[string]string{
-			"1": "01000000",
-			"2": "01000000",
-			"3": "01000000",
-			"4": "01000000",
-			"5": "01000000",
-			"6": "01000000",
+			"0": "01000000",
+			"1": "02000000",
+			"2": "03000000",
+			"3": "04000000",
+			"4": "05000000",
+			"5": "06000000",
 		},
 	}
 	return prado.NewDefaultPrado(config, "path")
+}
+
+func getStringCode(s string) mat.Matrix {
+	out := mat.NewEmptyVecDense(30)
+	c := 0
+	for _, char := range s {
+		if c < 30 {
+			for n := 1; n <= 3; n++ {
+				out.Data()[c] = float64(digit(int(char), n))
+				c++
+			}
+		}
+	}
+	return out.ProdScalar(0.1)
+}
+
+func digit(num, place int) int {
+	r := num % int(math.Pow(10, float64(place)))
+	return r / int(math.Pow(10, float64(place-1)))
+}
+
+func getHashedVocabulary(vocabulary *vocabulary.Vocabulary) map[string]mat.Matrix {
+	var outMap map[string]mat.Matrix
+	outMap = make(map[string]mat.Matrix)
+	for _, word := range vocabulary.Items() {
+		outMap[word] = getStringCode(word)
+	}
+	return outMap
 }
--- examples/text_classifier/trainer/trainer.go
+++ examples/text_classifier/trainer/trainer.go
@@ -7,11 +7,17 @@ package trainer
 import (
 	"bufio"
 	"fmt"
+	"github.com/gosuri/uiprogress"
 	"github.com/nlpodyssey/spago/pkg/mat/rand"
+	"github.com/nlpodyssey/spago/pkg/ml/ag"
+	"github.com/nlpodyssey/spago/pkg/ml/losses"
+	"github.com/nlpodyssey/spago/pkg/ml/nn"
 	"github.com/nlpodyssey/spago/pkg/ml/optimizers/gd"
 	"github.com/nlpodyssey/spago/pkg/nlp/classifiers/prado"
 	"github.com/nlpodyssey/spago/pkg/nlp/tokenizers"
-	"github.com/nlpodyssey/spago/pkg/nlp/tokenizers/wordpiecetokenizer"
+	"github.com/nlpodyssey/spago/pkg/nlp/tokenizers/basetokenizer"
+	"github.com/nlpodyssey/spago/pkg/nlp/vocabulary"
+	"github.com/nlpodyssey/spago/pkg/utils"
 	"io"
 	"os"
 )
@@ -33,6 +39,11 @@ type Trainer struct {
 	lastBatchLoss float64
 	model         *prado.Model
 	countLine     int
+	curLoss       float64
+	curEpoch      int
+	includeTitle  bool
+	includeBody   bool
+	labelsMap     map[string]int
 }
 
 func NewTrainer(model *prado.Model, config TrainingConfig, optimizer *gd.GradientDescent) *Trainer {
@@ -41,28 +52,80 @@ func NewTrainer(model *prado.Model, config TrainingConfig, optimizer *gd.Gradien
 		randGen:        rand.NewLockedRand(config.Seed),
 		optimizer:      optimizer,
 		model:          model,
+		includeBody:    false,
+		includeTitle:   true,
+		labelsMap: map[string]int{
+			"01000000": 0,
+			"02000000": 1,
+			"03000000": 2,
+			"04000000": 3,
+			"05000000": 4,
+			"06000000": 5},
 	}
 }
 
-func (t *Trainer) Train() {
-	t.forEachLine(func(i int, text string) {
+func (t *Trainer) GetTokenizedExample(e Example) []string {
+	out := []string{}
+	if t.includeTitle {
+		tokenized := t.tokenize(e.Title)
+		if len(tokenized) > 0 {
+			out = append(out, tokenized...)
+		}
+	}
+	if t.includeBody {
+		tokenized := t.tokenize(e.Title)
+		if len(tokenized) > 0 {
+			out = append(out, tokenized...)
+		}
+	}
+	return out
+}
+
+func (t *Trainer) GetVocabulary() *vocabulary.Vocabulary {
+	out := vocabulary.New([]string{})
+	err := t.forEachLine(func(i int, text string) {
+		e := GetExample(text)
+		tokenizedExample := t.GetTokenizedExample(e)
+		for _, word := range tokenizedExample {
+			out.Add(word)
+		}
+		t.countLine++
+	})
+	if err != nil && err != io.EOF {
+		fmt.Printf(" > Failed with error: %v\n", err)
+	}
+	return out
+}
+
+func (t *Trainer) trainBatches(onExample func()) {
+	err := t.forEachLine(func(i int, text string) {
 		//t.trainPassage(text)
 		e := GetExample(text)
-		println(e.Category)
-		tokenized := t.tokenize(e.Title)
-		println(tokenized)
+		tokenizedExample := t.GetTokenizedExample(e)
+		t.curLoss = t.learn(i, tokenizedExample, t.labelsMap[e.Category])
 		t.optimizer.IncBatch()
 		t.optimizer.IncExample()
 		t.optimizer.Optimize()
-
-		t.countLine++
+		onExample()
 	})
+	if err != nil && err != io.EOF {
+		fmt.Printf(" > Failed with error: %v\n", err)
+	}
+}
+
+// learn performs the backward respect to the cross-entropy loss, returned as scalar value
+func (t *Trainer) learn(_ int, tokenizedExample []string, label int) float64 {
+	g := ag.NewGraph(ag.Rand(rand.NewLockedRand(t.Seed)))
+	defer g.Clear()
+	y := t.model.NewProc(g).(*prado.Processor).Classify(tokenizedExample)[0]
+	loss := g.Div(losses.CrossEntropy(g, y, label), g.NewScalar(1.0))
+	g.Backward(loss)
+	return loss.ScalarValue()
 }
 
 func (t *Trainer) tokenize(text string) []string {
-	tokenizer := wordpiecetokenizer.New(t.model.Vocabulary)
-	tokenized := append(tokenizers.GetStrings(tokenizer.Tokenize(text)), wordpiecetokenizer.DefaultSequenceSeparator)
-	return append([]string{wordpiecetokenizer.DefaultClassToken}, tokenized...)
+	tokenizer := basetokenizer.New()
+	return tokenizers.GetStrings(tokenizer.Tokenize(text))
 }
 
 func (t *Trainer) forEachLine(callback func(i int, line string)) (err error) {
@@ -81,7 +144,6 @@ func (t *Trainer) forEachLine(callback func(i int, line string)) (err error) {
 		if err != nil && err != io.EOF {
 			break
 		}
-
 		// Process the line here
 		callback(i, line)
 		if err != nil {
@@ -93,4 +155,39 @@ func (t *Trainer) forEachLine(callback func(i int, line string)) (err error) {
 		return err
 	}
 	return
+}
+
+func (t *Trainer) newTrainBar(progress *uiprogress.Progress, nexamples int) *uiprogress.Bar {
+	bar := progress.AddBar(nexamples)
+	bar.AppendCompleted().PrependElapsed()
+	bar.PrependFunc(func(b *uiprogress.Bar) string {
+		return fmt.Sprintf("Epoch: %d Loss: %.6f", t.curEpoch, t.curLoss)
+	})
+	return bar
+}
+
+func (t *Trainer) Enjoy() {
+	for epoch := 0; epoch < t.Epochs; epoch++ {
+		t.curEpoch = epoch
+		t.optimizer.IncEpoch()
+
+		fmt.Println("Training epoch...")
+		t.trainEpoch()
+
+		// evaluate here
+
+		// model serialization
+		err := utils.SerializeToFile(t.ModelPath, nn.NewParamsSerializer(t.model))
+		if err != nil {
+			panic("mnist: error during model serialization.")
+		}
+	}
+}
+
+func (t *Trainer) trainEpoch() {
+	uip := uiprogress.New()
+	bar := t.newTrainBar(uip, t.countLine)
+	uip.Start() // start bar rendering
+	defer uip.Stop()
+	t.trainBatches(func() { bar.Incr() })
 }
