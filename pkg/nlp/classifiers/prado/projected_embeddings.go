ce573f09e3e2487e7d56e9b97d4b0d5dbf14ed8c
Edoardo Brambilla <e.brambilla@advanced-analytics.eu>
2021-01-13 15:27:30 +0100

    Modify projected embeddings

--- pkg/nlp/classifiers/prado/projected_embeddings.go
+++ pkg/nlp/classifiers/prado/projected_embeddings.go
@@ -1,7 +1,7 @@
 package prado
 
 import (
-	"github.com/nlpodyssey/spago/pkg/mat"
+	"github.com/nlpodyssey/spago/pkg/mat32"
 	"github.com/nlpodyssey/spago/pkg/ml/ag"
 	"github.com/nlpodyssey/spago/pkg/ml/encoding/hashing"
 	"github.com/nlpodyssey/spago/pkg/ml/nn"
@@ -12,8 +12,7 @@ import (
 )
 
 var (
-	_ nn.Model     = &Embeddings{}
-	_ nn.Processor = &EmbeddingsProcessor{}
+	_ nn.Model = &Embeddings{}
 )
 
 type EmbeddingsConfig struct {
@@ -25,6 +24,7 @@ type EmbeddingsConfig struct {
 }
 
 type Embeddings struct {
+	nn.BaseModel
 	EmbeddingsConfig
 	Word       *embeddings.Model
 	Projection *hashing.Data
@@ -47,48 +47,48 @@ func NewPradoEmbeddings(config EmbeddingsConfig) *Embeddings {
 	}
 }
 
-type EmbeddingsProcessor struct {
-	nn.BaseProcessor
-	model               *Embeddings
-	wordsLayer          *embeddings.Processor
-	tokenTypeEmbeddings []ag.Node
-	unknownEmbedding    ag.Node
-}
+//type EmbeddingsProcessor struct {
+//	nn.BaseProcessor
+//	model               *Embeddings
+//	wordsLayer          *embeddings.Processor
+//	tokenTypeEmbeddings []ag.Node
+//	unknownEmbedding    ag.Node
+//}
 
 // Set Embeddings from a map of string into matrix. Codes may be calculated from string ascii, bytes or
 // other type of embeddings
-func (m *Embeddings) SetProjectedEmbeddings(codes map[string]mat.Matrix) {
+func (m *Embeddings) SetProjectedEmbeddings(codes map[string]mat32.Matrix) {
 	for word, code := range codes {
 		hashedCode := m.Projection.GetHash(code)
 		m.Word.SetEmbeddingFromData(word, hashedCode.Data())
 	}
 }
 
-func (m *Embeddings) NewProc(ctx nn.Context) nn.Processor {
-	graph := ctx.Graph
-	return &EmbeddingsProcessor{
-		BaseProcessor: nn.BaseProcessor{
-			Model:             m,
-			Mode:              nn.Training,
-			Graph:             graph,
-			FullSeqProcessing: false,
-		},
-		model:            m,
-		wordsLayer:       m.Word.NewProc(ctx).(*embeddings.Processor),
-		unknownEmbedding: graph.NewWrap(graph.NewVariable(m.Projection.GetHash(mat.NewInitDense(m.Size, 1, -1.0)), false)),
-	}
-}
+//func (m *Embeddings) NewProc(ctx nn.Context) nn.Processor {
+//	graph := ctx.Graph
+//	return &EmbeddingsProcessor{
+//		BaseProcessor: nn.BaseProcessor{
+//			Model:             m,
+//			Mode:              nn.Training,
+//			Graph:             graph,
+//			FullSeqProcessing: false,
+//		},
+//		model:            m,
+//		wordsLayer:       m.Word.NewProc(ctx).(*embeddings.Processor),
+//		unknownEmbedding: graph.NewWrap(graph.NewVariable(m.Projection.GetHash(mat.NewInitDense(m.Size, 1, -1.0)), false)),
+//	}
+//}
 
-func (p *EmbeddingsProcessor) EmbedSequence(words []string) []ag.Node {
+func (p *Embeddings) EmbedSequence(words []string) []ag.Node {
 	encoded := make([]ag.Node, len(words))
 	wordEmbeddings := p.getWordEmbeddings(words)
 	sequenceIndex := 0
 	for i := 0; i < len(words); i++ {
 		if wordEmbeddings[i] != nil {
-			encoded[i] = p.Graph.NewWrapNoGrad(wordEmbeddings[i])
+			encoded[i] = p.Graph().NewWrapNoGrad(wordEmbeddings[i])
 		} else {
 			code := getHashCode(words[i])
-			encoded[i] = p.Graph.NewVariable(p.model.Projection.GetHash(code), false)
+			encoded[i] = p.Graph().NewVariable(p.Projection.GetHash(code), false)
 		}
 		if words[i] == wordpiecetokenizer.DefaultSequenceSeparator {
 			sequenceIndex++
@@ -97,11 +97,11 @@ func (p *EmbeddingsProcessor) EmbedSequence(words []string) []ag.Node {
 	return encoded
 }
 
-func getHashCode(s string) mat.Matrix {
-	out := mat.NewEmptyVecDense(30)
+func getHashCode(s string) mat32.Matrix {
+	out := mat32.NewEmptyVecDense(30)
 	c := 0
 	for i := 0; i < 30; i++ {
-		out.Data()[c] = (rand.Float64() * 2.0) - 1.0
+		out.Data()[c] = (rand.Float32() * 2.0) - 1.0
 		c++
 	}
 	return out //.ProdScalar(0.1)
@@ -112,10 +112,10 @@ func digit(num, place int) int {
 	return r / int(math.Pow(10, float64(place-1)))
 }
 
-func (p *EmbeddingsProcessor) getWordEmbeddings(words []string) []ag.Node {
-	return p.wordsLayer.Encode(words)
+func (p *Embeddings) getWordEmbeddings(words []string) []ag.Node {
+	return p.Word.Encode(words)
 }
 
-func (p *EmbeddingsProcessor) Forward(_ ...ag.Node) []ag.Node {
-	panic("Prado: Forward() method not implemented. Use EmbedSequence() instead.")
-}
+//func (p *EmbeddingsProcessor) Forward(_ ...ag.Node) []ag.Node {
+//	panic("Prado: Forward() method not implemented. Use EmbedSequence() instead.")
+//}
