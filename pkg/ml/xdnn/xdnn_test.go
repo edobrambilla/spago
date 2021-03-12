ff60b628675dc4ac596d0250370b5db7b5ec4bc5
Edoardo Brambilla <e.brambilla@advanced-analytics.eu>
2021-03-12 15:04:44 +0100

    Fix test

--- pkg/ml/xdnn/xdnn_test.go
+++ pkg/ml/xdnn/xdnn_test.go
@@ -137,14 +137,20 @@ func Test_CheckExample(t *testing.T) {
 	a[0] = mat32.NewVecDense([]float32{-2.2, 3.4, 3.2, 2.9, -0.6, 4.3})
 	a[1] = mat32.NewVecDense([]float32{-0.3, 1.5, 0.9, -2.5, 0.4, 9.3})
 	a[2] = mat32.NewVecDense([]float32{0.0, 2.4, -0.3, -0.2, -0.1, 1.5})
-	a[3] = mat32.NewVecDense([]float32{0.0, 2.4, -0.3, -0.2, -0.1, 1.5})
-	a[4] = mat32.NewVecDense([]float32{0.0, 2.4, -0.3, -0.2, -0.1, 1.5})
+	a[3] = mat32.NewVecDense([]float32{0.3, 2.6, -1.0, 2.8, 3.5, -1.4})
+	a[4] = mat32.NewVecDense([]float32{-0.6, -0.2, 0.5, 0.8, -3.0, -1.0})
 	model := simpleXDNN()
 	model.CheckExample(a[0], 0, 0)
 	assert.Equal(t, model.Classes[0].Prototypes, 1)
 	assert.Equal(t, model.Classes[0].PrototypesSupport[0], 1)
 	assert.Equal(t, model.Classes[0].PrototypesVectors[0], mat32.NewVecDense([]float32{-2.2, 3.4, 3.2, 2.9, -0.6, 4.3}))
 	assert.Equal(t, model.Classes[0].Mean, mat32.NewVecDense([]float32{-2.2, 3.4, 3.2, 2.9, -0.6, 4.3}))
+	model.CheckExample(a[1], 1, 0)
+	assert.Equal(t, model.Classes[0].Prototypes, 2)
+	assert.Equal(t, model.Classes[0].PrototypesSupport[0], 1)
+	assert.Equal(t, model.Classes[0].PrototypesSupport[1], 1)
+	assert.Equal(t, model.Classes[0].PrototypesVectors[0], mat32.NewVecDense([]float32{-2.2, 3.4, 3.2, 2.9, -0.6, 4.3}))
+	assert.Equal(t, model.Classes[0].PrototypesVectors[1], mat32.NewVecDense([]float32{-0.3, 1.5, 0.9, -2.5, 0.4, 9.3}))
 }
 
 func assertEqualApprox(t *testing.T, expected, actual float32) {
