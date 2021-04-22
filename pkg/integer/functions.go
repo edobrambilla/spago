d27ffe0ef03f2f210c38884641e9e80f3c586882
Edoardo Brambilla <e.brambilla@advanced-analytics.eu>
2021-04-22 17:43:15 +0200

    Add integer gelu test

--- pkg/integer/functions.go
+++ pkg/integer/functions.go
@@ -15,6 +15,11 @@ type Quantization struct {
 	scaling float32
 }
 
+type QuantizedInt struct {
+	q       int
+	scaling float32
+}
+
 func NewQuantization(b int, clip float32) Quantization {
 	scaling := clip / (mat.Pow(2.0, float32(b)) - 1)
 	return Quantization{b, clip, scaling}
@@ -32,4 +37,46 @@ func (q *Quantization) Quantize(x float32) int {
 
 func (q *Quantization) Dequantize(x int) float32 {
 	return float32(x) * q.scaling
+}
+
+func (q *Quantization) integerPoly(a, b, c float32, input int) QuantizedInt {
+	qb := int(math.Floor(float64(b / q.scaling)))
+	qc := int(math.Floor(float64(c / (a * q.scaling * q.scaling))))
+	scalingOut := a * q.scaling * q.scaling
+	qOut := ((input + qb) * (input + qb)) + qc
+	return QuantizedInt{qOut, scalingOut}
+}
+
+func (q *Quantization) integerErf(input int) QuantizedInt {
+	a := float32(-0.28888)
+	b := float32(-1.769)
+	c := float32(1.0)
+	var qsgn = 1
+	qtmp := Quantization{q.b, math.MaxFloat32, q.scaling}
+	if input > 0 {
+		if input > (int(-b / q.scaling)) {
+			input = int(-b / q.scaling)
+		}
+	} else {
+		qsgn = -1
+		if -input > (int(-b / q.scaling)) {
+			input = -int(-b / q.scaling)
+		} else {
+			input = -input
+		}
+	}
+	qL := qtmp.integerPoly(a, b, c, input)
+	qOut := qsgn * qL.q
+	scalingOut := qL.scaling
+	return QuantizedInt{qOut, scalingOut}
+}
+
+func (q *Quantization) IntegerGelu(input int) QuantizedInt {
+	qtmp := Quantization{q.b, math.MaxFloat32, q.scaling / 1.4142135624}
+	qErf := qtmp.integerErf(input)
+	qOne := int(math.Floor(float64(1.0 / qErf.scaling)))
+	qOut := input * (qErf.q + qOne)
+	scalingOut := q.scaling * qErf.scaling / 2
+
+	return QuantizedInt{qOut, scalingOut}
 }
--- pkg/integer/functions_test.go
+++ pkg/integer/functions_test.go
@@ -16,3 +16,16 @@ func TestQuantization_Quantize(t *testing.T) {
 	x := q.Dequantize(a)
 	assert.InDelta(t, x, 3.5678, 1.0e-1)
 }
+
+func TestQuantiztion_IntegerGelu(t *testing.T) {
+	q := NewQuantization(7, 50)
+	a := q.Quantize(0.55)
+	assert.Equal(t, a, 1)
+	gelu := q.IntegerGelu(a)
+	assert.Equal(t, gelu.q, -54)
+	assert.Equal(t, gelu.scaling, float32(-0.0044071344))
+	b := q.Quantize(-1)
+	gelu = q.IntegerGelu(b)
+	assert.Equal(t, gelu.q, 48)
+	assert.Equal(t, gelu.scaling, float32(-0.0044071344))
+}
